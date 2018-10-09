89364c5a555879a869d27c6479e48b0a14298503
Lucien <lygm1062@gmail.com>
2018-10-09 16:43:44 +0800

    update

---
+++ precache-manifest.ddc755f57a0e53af90eaf0c699f34ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "13844fc7f65b11ca91aa",
+    "url": "/kijudgs/static/js/runtime~main.13844fc7.js"
+  },
+  {
+    "revision": "c78499a21d798478eb6a",
+    "url": "/kijudgs/static/js/main.c78499a2.chunk.js"
+  },
+  {
+    "revision": "36d0b5c4912296c01f8d",
+    "url": "/kijudgs/static/js/1.36d0b5c4.chunk.js"
+  },
+  {
+    "revision": "c78499a21d798478eb6a",
+    "url": "/kijudgs/static/css/main.448a944c.chunk.css"
+  },
+  {
+    "revision": "36d0b5c4912296c01f8d",
+    "url": "/kijudgs/static/css/1.81f6140b.chunk.css"
+  },
+  {
+    "revision": "88a837b19ffc2086cd8be6bf7170bf03",
+    "url": "/kijudgs/index.html"
+  }
+];
